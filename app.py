0c0ff4be947b3a74fbf389315e2ee7e9e18be58e
felipegrabowsky <fm2579@columbia.edu>
2018-01-17 21:48:26 -0500

    Labs Application Commit: Created my own website Extended Flask App Created my own website Extended Flask AppLabs Application Commit: Created my own website Extended Flask App    app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/fm2579', methods=['GET'])
+def main():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run()
